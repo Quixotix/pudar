05a8938a3d8202cb068f599b5672e3eace2b2785
Ryland Rene Taylor-Almanza <ryland@quixotix.com>
2013-12-14 11:23:42 -0800

    Made a blue screen. You only git one screen. Ha!

---
+++ js/game.js
@@ -0,0 +1,10 @@
+Game = {
+    // Initialize crafty and start our game
+    start: function() {
+        // Tiles will be 16x16 pixels, and a screen will have 40x30 tiles.
+        // This comes out to a screen size of 640x480
+        Crafty.init(640, 480);
+        // Set the screens background to Micah's favorite color
+        Crafty.background('blue');
+    }
+}
