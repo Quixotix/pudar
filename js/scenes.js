7db101397dd5f485a9bc941309dd1057d08eb24b
Micah Carrick <micah@janrain.com>
2013-12-14 19:18:19 -0800

    Improved collision

--- js/scenes.js
+++ js/scenes.js
@@ -19,7 +19,7 @@ Crafty.scene('Game', function() {
                  collision < map.getEntitiesInLayer('collision').length;
                  collision++) {
                 map.getEntitiesInLayer('collision')[collision]
-                    .addComponent("Collision, Solid")
+                    .addComponent("Collision")
                     .collision();
             }
 
